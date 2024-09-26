ee760f31a011e8ab9269b624eb9f98d8c728dd76
Ufo123CZ <ufo123hry@seznam.cz>
2024-09-26 13:14:01 +0200

    Server Class (Client Side)

---
+++ Dice_Client/src/main/java/usp_sp/Server/Connection.java
@@ -0,0 +1,35 @@
+package usp_sp.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Connection {
+
+    public Connection() throws IOException {
+        connectToServer();
+    }
+
+    public void connectToServer() throws IOException {
+        // create a socket to connect to the server running on localhost at port number 9090
+        Socket socket = new Socket("localhost", 9090);
+
+        // Setup output stream to send data to the server
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Setup input stream to receive data from the server
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        // Send message to the server
+        out.println("Hello from client!");
+
+        // Receive response from the server
+        String response = in.readLine();
+        System.out.println("Server says: " + response);
+
+        // Close the socket
+        socket.close();
+    }
+}
